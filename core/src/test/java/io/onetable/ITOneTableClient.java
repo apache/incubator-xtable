a1a1f90430722509ce4ab00d03c68d080fc6e263
Timothy Brown <tim@onehouse.ai>
2023-10-24 10:43:59 -0500

    use forked jvm to avoid spark session issues

--- core/src/test/java/io/onetable/ITOneTableClient.java
+++ core/src/test/java/io/onetable/ITOneTableClient.java
@@ -25,7 +25,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.time.ZoneId;
@@ -42,16 +41,12 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import lombok.SneakyThrows;
 import org.apache.avro.Schema;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.catalyst.encoders.RowEncoder;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import org.junit.jupiter.api.AfterAll;
@@ -108,21 +103,6 @@ public class ITOneTableClient {
         .hadoopConfiguration()
         .set("parquet.avro.write-old-list-structure", "false");
     jsc = JavaSparkContext.fromSparkContext(sparkSession.sparkContext());
-    // The default_iceberg catalog can fail to load properly when tests are run in parallel, so we
-    // force it to initialize here
-    forceInitIcebergCatalog();
-  }
-
-  @SneakyThrows
-  private static void forceInitIcebergCatalog() {
-    StructType tmpSchema = new StructType(new StructField[]{new StructField("dummy_field", DataTypes.LongType, false, Metadata.empty())});
-    Path tmpBasePath = tempDir.resolve("tmp");
-    Files.createDirectories(tmpBasePath);
-    sparkSession
-        .emptyDataset(RowEncoder.apply(tmpSchema))
-        .write()
-        .format("iceberg")
-        .save(tmpBasePath.toString());
   }
 
   @BeforeEach
