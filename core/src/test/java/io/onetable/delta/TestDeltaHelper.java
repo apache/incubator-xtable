c4df46ca45d29f16bc158269ced1b21333df4a03
Timothy Brown <tim@onehouse.ai>
2023-10-29 13:56:02 -0500

    add more types for delta source testing

--- core/src/test/java/io/onetable/delta/TestDeltaHelper.java
+++ core/src/test/java/io/onetable/delta/TestDeltaHelper.java
@@ -18,29 +18,41 @@
  
 package io.onetable.delta;
 
+import static org.apache.spark.sql.types.DataTypes.BinaryType;
+import static org.apache.spark.sql.types.DataTypes.BooleanType;
+import static org.apache.spark.sql.types.DataTypes.DateType;
+import static org.apache.spark.sql.types.DataTypes.DoubleType;
+import static org.apache.spark.sql.types.DataTypes.FloatType;
 import static org.apache.spark.sql.types.DataTypes.IntegerType;
+import static org.apache.spark.sql.types.DataTypes.LongType;
 import static org.apache.spark.sql.types.DataTypes.StringType;
 import static org.apache.spark.sql.types.DataTypes.TimestampType;
 
+import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
-import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Value;
+import lombok.Getter;
 
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.ArrayType;
+import org.apache.spark.sql.types.DataType;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.MapType;
 import org.apache.spark.sql.types.Metadata;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
@@ -49,16 +61,31 @@ import io.delta.tables.DeltaTable;
 import io.delta.tables.DeltaTableBuilder;
 
 @Builder
-@Value
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TestDeltaHelper {
+  private static final StructType STRUCT_SCHEMA = DataTypes.createStructType(new StructField[] {
+      new StructField("nested_int", IntegerType, true, Metadata.empty())
+  });
+
   private static final StructField[] COMMON_FIELDS =
       new StructField[] {
         new StructField("id", IntegerType, false, Metadata.empty()),
         new StructField("firstName", StringType, true, Metadata.empty()),
         new StructField("lastName", StringType, true, Metadata.empty()),
-        new StructField("gender", StringType, true, Metadata.empty()),
-        new StructField("birthDate", TimestampType, true, Metadata.empty())
+        new StructField("boolean_field", BooleanType, true, Metadata.empty()),
+        new StructField("date_field", DateType, true, Metadata.empty()),
+        new StructField("timestamp_field", TimestampType, true, Metadata.empty()),
+        new StructField("double_field", DoubleType, true, Metadata.empty()),
+        new StructField("float_field", FloatType, true, Metadata.empty()),
+        new StructField("long_field", LongType, true, Metadata.empty()),
+        new StructField("binary_field", BinaryType, true, Metadata.empty()),
+        new StructField("primitive_map", DataTypes.createMapType(StringType, IntegerType), true, Metadata.empty()),
+        new StructField("record_map", DataTypes.createMapType(StringType, STRUCT_SCHEMA), true, Metadata.empty()),
+        new StructField("primitive_list", DataTypes.createArrayType(IntegerType), true, Metadata.empty()),
+        new StructField("record_list", DataTypes.createArrayType(STRUCT_SCHEMA), true, Metadata.empty()),
+        new StructField("record_field", STRUCT_SCHEMA, true, Metadata.empty()),
+        new StructField("birthDate", TimestampType, true, Metadata.empty()),
       };
   private static final StructField[] COMMON_DATE_FIELDS =
       new StructField[] {new StructField("birthDate", TimestampType, true, Metadata.empty())};
@@ -68,7 +95,6 @@ public class TestDeltaHelper {
       new StructField[] {new StructField("yearOfBirth", IntegerType, true, Metadata.empty())};
 
   private static final Random RANDOM = new Random();
-  private static final String[] GENDERS = {"Male", "Female"};
 
   StructType tableStructSchema;
   boolean tableIsPartitioned;
@@ -100,12 +126,8 @@ public class TestDeltaHelper {
     DeltaTableBuilder tableBuilder =
         DeltaTable.createIfNotExists(sparkSession)
             .tableName(tableName)
-            .location(basePath)
-            .addColumn("id", IntegerType)
-            .addColumn("firstName", StringType)
-            .addColumn("lastName", StringType)
-            .addColumn("gender", StringType)
-            .addColumn("birthDate", TimestampType);
+            .location(basePath);
+    Arrays.stream(COMMON_FIELDS).forEach(tableBuilder::addColumn);
     if (tableIsPartitioned) {
       tableBuilder
           .addColumn(
@@ -146,18 +168,49 @@ public class TestDeltaHelper {
     switch (field.name()) {
       case "id":
         return RANDOM.nextInt(1000000) + 1;
-      case "firstName":
-      case "lastName":
-      case "street":
-        return generateRandomString();
-      case "gender":
-        return GENDERS[RANDOM.nextInt(GENDERS.length)];
       case "birthDate":
         return generateRandomTimeGivenYear(yearValue);
       case "yearOfBirth":
         return yearValue;
       default:
-        throw new IllegalArgumentException("Please add code to handle field: " + field.name());
+        return getRandomValueForField(field.dataType(), field.nullable());
+    }
+  }
+
+  private static Object getRandomValueForField(DataType type, boolean isNullable) {
+    if (isNullable && RANDOM.nextBoolean()) {
+      return null;
+    }
+    switch (type.typeName()) {
+      case "integer":
+        return RANDOM.nextInt();
+      case "string":
+        return generateRandomString();
+      case "boolean":
+        return RANDOM.nextBoolean();
+      case "float":
+        return RANDOM.nextFloat();
+      case "double":
+        return RANDOM.nextDouble();
+      case "binary":
+        return generateRandomString().getBytes();
+      case "long":
+        return RANDOM.nextLong();
+      case "date":
+        return new Date(System.currentTimeMillis());
+      case "timestamp":
+        return Timestamp.from(Instant.now());
+      case "array":
+        ArrayType arrayType = (ArrayType) type;
+        return IntStream.range(0, RANDOM.nextInt(5)).mapToObj(i -> getRandomValueForField(arrayType.elementType(), false)).collect(Collectors.toList());
+      case "map":
+        MapType mapType = (MapType) type;
+        return IntStream.range(0, RANDOM.nextInt(5)).mapToObj(i -> getRandomValueForField(mapType.valueType(), false)).collect(Collectors.toMap(unused -> generateRandomString(), Function.identity()));
+      case "struct":
+        StructType structType = (StructType) type;
+        return RowFactory.create(Arrays.stream(structType.fields()).map(field -> getRandomValueForField(field.dataType(), false)).toArray());
+      default:
+        throw new IllegalArgumentException("Please add code to handle field type: " + type);
     }
   }
 
