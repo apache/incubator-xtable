ce12ab4e600ad80bc3aa6583838ad45301e2422e
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-10-31 13:40:45 -0700

    minor changes in docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/docs',
+//          routeBasePath: '/docs',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
