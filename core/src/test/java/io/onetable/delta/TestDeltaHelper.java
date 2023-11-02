b11885312744deaddeea146204d6a262c6ac481a
Vamshi Gudavarthi <vamshi@onehouse.ai>
2023-11-02 12:37:53 -0700

    some adjustments

--- core/src/test/java/io/onetable/delta/TestDeltaHelper.java
+++ core/src/test/java/io/onetable/delta/TestDeltaHelper.java
@@ -247,11 +247,9 @@ public class TestDeltaHelper {
 
   private Timestamp generateRandomTimeGivenYear(int yearValue) {
     int month = RANDOM.nextInt(12) + 1;
-    int daysInMonth = YearMonth.of(yearValue, month).lengthOfMonth();
-    // Adjust days in December to avoid timezone issues with Delta generated columns check
-    // constraint for tests.
-    int maxDay = month == 12 ? daysInMonth - 5 : daysInMonth;
-    int day = RANDOM.nextInt(maxDay) + 1;
+    // Adjust days to avoid timezone issues with Delta generated columns check constraint for tests.
+    int daysToConsider = YearMonth.of(yearValue, month).lengthOfMonth() - 5;
+    int day = RANDOM.nextInt(daysToConsider) + 1;
 
     LocalDateTime localDateTime =
         LocalDateTime.of(
