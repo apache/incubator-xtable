71131f33ebb382e541200502ba489dd994676d05
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-11-25 09:50:09 -0800

    addresses 257

--- demo/start_demo.sh
+++ demo/start_demo.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Create the require jars for the demo and copy them into a directory we'll mount in our notebook container
-cd .. && mvn install -am -pl core -DskipTests -T 2
+# Create the require jars for the demo and copy them into a directory we'll mount in our notebook & spark container
+cd .. && mvn install -DskipTests -T 2
 mkdir -p demo/jars
 cp hudi-support/utils/target/hudi-utils-0.1.0-SNAPSHOT.jar demo/jars
 cp api/target/onetable-api-0.1.0-SNAPSHOT.jar demo/jars
 cp core/target/onetable-core-0.1.0-SNAPSHOT.jar demo/jars
+cp utilities/target/utilities-0.1.0-SNAPSHOT-bundled.jar demo/jars
 
 cd demo
 docker-compose up
