9c90c85627588db4c358e1bc8654b7fc5fc6a3f2
Sagar Lakshmipathy <18vidhyasagar@gmail.com>
2023-12-07 01:34:57 -0800

    removed duration component

--- website/src/theme/BlogPostItem/Header/Info/index.js
+++ website/src/theme/BlogPostItem/Header/Info/index.js
@@ -1,32 +1,8 @@
 import React from 'react';
 import clsx from 'clsx';
-import {translate} from '@docusaurus/Translate';
-import {usePluralForm} from '@docusaurus/theme-common';
 import {useBlogPost} from '@docusaurus/theme-common/internal';
 import styles from './styles.module.css';
-// Very simple pluralization: probably good enough for now
-function useReadingTimePlural() {
-  const {selectMessage} = usePluralForm();
-  return (readingTimeFloat) => {
-    const readingTime = Math.ceil(readingTimeFloat);
-    return selectMessage(
-      readingTime,
-      translate(
-        {
-          id: 'theme.blog.post.readingTime.plurals',
-          description:
-            'Pluralized label for "{readingTime} min read". Use as much plural forms (separated by "|") as your language support (see https://www.unicode.org/cldr/cldr-aux/charts/34/supplemental/language_plural_rules.html)',
-          message: 'One min read|{readingTime} min read',
-        },
-        {readingTime},
-      ),
-    );
-  };
-}
-function ReadingTime({readingTime}) {
-  const readingTimePlural = useReadingTimePlural();
-  return <>{readingTimePlural(readingTime)}</>;
-}
+
 function Date({date, formattedDate}) {
   return (
     <time dateTime={date} itemProp="datePublished">
@@ -34,21 +10,13 @@ function Date({date, formattedDate}) {
     </time>
   );
 }
-function Spacer() {
-  return <>{' · '}</>;
-}
+
 export default function BlogPostItemHeaderInfo({className}) {
   const {metadata} = useBlogPost();
-  const {date, formattedDate, readingTime} = metadata;
+  const {date, formattedDate} = metadata;
   return (
     <div className={clsx(styles.container, 'margin-vert--md', className)}>
       <Date date={date} formattedDate={formattedDate} />
-      {typeof readingTime !== 'undefined' && (
-        <>
-          <Spacer />
-          <ReadingTime readingTime={readingTime} />
-        </>
-      )}
     </div>
   );
 }
