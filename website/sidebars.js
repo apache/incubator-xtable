cd04bac1d187d57a7e2dc73a4c163462fd0e3421
Albert T. Wong <atwong@alumni.uci.edu>
2024-02-01 13:04:01 -0800

    adding in StarRocks connection information (#315)
    Signed-off-by: alberttwong <atwong@alumni.uci.edu>
    
    Update starrocks.md
    
    correcting syntax for starrocks
    
    Update sidebars.js
    
    adding starrocks page.

--- website/sidebars.js
+++ website/sidebars.js
@@ -57,6 +57,7 @@ module.exports = {
                         'fabric',
                         'presto',
                         'snowflake',
+                        'starrocks',
                         'trino',
                     ],
                 }
