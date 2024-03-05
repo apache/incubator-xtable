124406ae4f649c3285e0dd5a3d93ebc8f93a48a5
Kyle Weller <kywe665@gmail.com>
2024-03-05 00:26:26 -0800

    caught docusaurus config meta title

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'XTable',
+  title: 'Apache XTable™',
   favicon: 'images/xtable-favicon.png',
   url: 'https://onetable.dev',
   baseUrl: '/',
@@ -45,7 +45,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'XTable',
+        title: 'Apache XTable™',
         logo: {
           alt: 'XTable Logo',
           href: 'pathname:///',
