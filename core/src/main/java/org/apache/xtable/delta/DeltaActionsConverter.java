apache/incubator-xtable

commit 243e981d9458d9fb731842ddaaa0b551bbfdff77

Repackaging code under the apache namespace
Closes #374

core/src/main/java/org/apache/xtable/delta/DeltaActionsConverter.java
@@ -1,0 +1,109 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.apache.xtable.delta;
+
+import java.util.Collections;
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import org.apache.hadoop.fs.Path;
+
+import org.apache.spark.sql.delta.Snapshot;
+import org.apache.spark.sql.delta.actions.AddFile;
+import org.apache.spark.sql.delta.actions.RemoveFile;
+
+import org.apache.xtable.exception.NotSupportedException;
+import org.apache.xtable.model.schema.OneField;
+import org.apache.xtable.model.schema.OnePartitionField;
+import org.apache.xtable.model.stat.ColumnStat;
+import org.apache.xtable.model.storage.FileFormat;
+import org.apache.xtable.model.storage.OneDataFile;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DeltaActionsConverter {
+
+  private static final DeltaActionsConverter INSTANCE = new DeltaActionsConverter();
+
+  public static DeltaActionsConverter getInstance() {
+    return INSTANCE;
+  }
+
+  public OneDataFile convertAddActionToOneDataFile(
+      AddFile addFile,
+      Snapshot deltaSnapshot,
+      FileFormat fileFormat,
+      List<OnePartitionField> partitionFields,
+      List<OneField> fields,
+      boolean includeColumnStats,
+      DeltaPartitionExtractor partitionExtractor,
+      DeltaStatsExtractor fileStatsExtractor) {
+    List<ColumnStat> columnStats =
+        includeColumnStats
+            ? fileStatsExtractor.getColumnStatsForFile(addFile, fields)
+            : Collections.emptyList();
+    long recordCount =
+        columnStats.stream().map(ColumnStat::getNumValues).max(Long::compareTo).orElse(0L);
+    // TODO(https://github.com/onetable-io/onetable/issues/102): removed record count.
+    return OneDataFile.builder()
+        .physicalPath(getFullPathToFile(deltaSnapshot, addFile.path()))
+        .fileFormat(fileFormat)
+        .fileSizeBytes(addFile.size())
+        .lastModified(addFile.modificationTime())
+        .partitionValues(
+            partitionExtractor.partitionValueExtraction(addFile.partitionValues(), partitionFields))
+        .columnStats(columnStats)
+        .recordCount(recordCount)
+        .build();
+  }
+
+  public OneDataFile convertRemoveActionToOneDataFile(
+      RemoveFile removeFile,
+      Snapshot deltaSnapshot,
+      FileFormat fileFormat,
+      List<OnePartitionField> partitionFields,
+      DeltaPartitionExtractor partitionExtractor) {
+    return OneDataFile.builder()
+        .physicalPath(getFullPathToFile(deltaSnapshot, removeFile.path()))
+        .fileFormat(fileFormat)
+        .partitionValues(
+            partitionExtractor.partitionValueExtraction(
+                removeFile.partitionValues(), partitionFields))
+        .build();
+  }
+
+  public FileFormat convertToOneTableFileFormat(String provider) {
+    if (provider.equals("parquet")) {
+      return FileFormat.APACHE_PARQUET;
+    } else if (provider.equals("orc")) {
+      return FileFormat.APACHE_ORC;
+    }
+    throw new NotSupportedException(
+        String.format("delta file format %s is not recognized", provider));
+  }
+
+  static String getFullPathToFile(Snapshot snapshot, String dataFilePath) {
+    String tableBasePath = snapshot.deltaLog().dataPath().toUri().toString();
+    if (dataFilePath.startsWith(tableBasePath)) {
+      return dataFilePath;
+    }
+    return tableBasePath + Path.SEPARATOR + dataFilePath;
+  }
+}

core/src/main/java/org/apache/xtable/delta/DeltaIncrementalChangesState.java
@@ -1,0 +1,104 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.apache.xtable.delta;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Builder;
+
+import org.apache.spark.sql.delta.DeltaLog;
+import org.apache.spark.sql.delta.actions.Action;
+
+import scala.Tuple2;
+import scala.collection.JavaConverters;
+import scala.collection.Seq;
+
+import com.google.common.base.Preconditions;
+
+/** Cache store for storing incremental table changes in the Delta table. */
+public class DeltaIncrementalChangesState {
+  private final Long startVersion;
+  private final Long endVersion;
+
+  private final Map<Long, List<Action>> incrementalChangesByVersion = new HashMap<>();
+
+  /**
+   * Reloads the cache store with incremental changes. Intentionally thread safety is the
+   * responsibility of the caller.
+   *
+   * @param deltaLog The DeltaLog instance.
+   * @param versionToStartFrom The version to start from.
+   */
+  @Builder
+  public DeltaIncrementalChangesState(DeltaLog deltaLog, Long versionToStartFrom) {
+    // TODO (https://github.com/onetable-io/onetable/issues/103) Fall back to snapshot sync in
+    // vacuum cases.
+    List<Tuple2<Long, List<Action>>> changesList =
+        getChangesList(deltaLog.getChanges(versionToStartFrom, false));
+    Long maxSeenVersion = null;
+    for (Tuple2<Long, List<Action>> change : changesList) {
+      Long versionNumber = change._1();
+      List<Action> actions = change._2();
+      incrementalChangesByVersion.put(versionNumber, actions);
+      maxSeenVersion =
+          maxSeenVersion == null ? versionNumber : Math.max(maxSeenVersion, versionNumber);
+    }
+    startVersion = versionToStartFrom;
+    endVersion = maxSeenVersion;
+  }
+
+  /**
+   * Returns the versions in sorted order. The start version is the next one after the last sync
+   * version to the target client. The end version is the latest version in the Delta table at the
+   * time of initialization.
+   *
+   * @return
+   */
+  public List<Long> getVersionsInSortedOrder() {
+    List<Long> versions = new ArrayList<>(incrementalChangesByVersion.keySet());
+    versions.sort(Long::compareTo);
+    return versions;
+  }
+
+  public List<Action> getActionsForVersion(Long version) {
+    Preconditions.checkArgument(
+        incrementalChangesByVersion.containsKey(version),
+        String.format("Version %s not found in the DeltaIncrementalChangesState.", version));
+    return incrementalChangesByVersion.get(version);
+  }
+
+  private List<Tuple2<Long, List<Action>>> getChangesList(
+      scala.collection.Iterator<Tuple2<Object, Seq<Action>>> scalaIterator) {
+    List<Tuple2<Long, List<Action>>> changesList = new ArrayList<>();
+    Iterator<Tuple2<Object, Seq<Action>>> javaIterator =
+        JavaConverters.asJavaIteratorConverter(scalaIterator).asJava();
+    while (javaIterator.hasNext()) {
+      Tuple2<Object, Seq<Action>> currentChange = javaIterator.next();
+      changesList.add(
+          new Tuple2<>(
+              (Long) currentChange._1(),
+              JavaConverters.seqAsJavaListConverter(currentChange._2()).asJava()));
+    }
+    return changesList;
+  }
+}

core/src/test/java/org/apache/xtable/testutil/Issues.java
@@ -1,0 +1,25 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.apache.xtable.testutil;
+
+public class Issues {
+
+  /** Refer to <a href="https://github.com/onetable-io/onetable/issues/102">GitHub #102</a> */
+  public static final boolean ISSUE_102_FIXED = false;
+}

core/src/main/java/io/onetable/delta/DeltaActionsConverter.java
@@ -1,109 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
-package io.onetable.delta;
-
-import java.util.Collections;
-import java.util.List;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-
-import org.apache.hadoop.fs.Path;
-
-import org.apache.spark.sql.delta.Snapshot;
-import org.apache.spark.sql.delta.actions.AddFile;
-import org.apache.spark.sql.delta.actions.RemoveFile;
-
-import io.onetable.exception.NotSupportedException;
-import io.onetable.model.schema.OneField;
-import io.onetable.model.schema.OnePartitionField;
-import io.onetable.model.stat.ColumnStat;
-import io.onetable.model.storage.FileFormat;
-import io.onetable.model.storage.OneDataFile;
-
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class DeltaActionsConverter {
-
-  private static final DeltaActionsConverter INSTANCE = new DeltaActionsConverter();
-
-  public static DeltaActionsConverter getInstance() {
-    return INSTANCE;
-  }
-
-  public OneDataFile convertAddActionToOneDataFile(
-      AddFile addFile,
-      Snapshot deltaSnapshot,
-      FileFormat fileFormat,
-      List<OnePartitionField> partitionFields,
-      List<OneField> fields,
-      boolean includeColumnStats,
-      DeltaPartitionExtractor partitionExtractor,
-      DeltaStatsExtractor fileStatsExtractor) {
-    List<ColumnStat> columnStats =
-        includeColumnStats
-            ? fileStatsExtractor.getColumnStatsForFile(addFile, fields)
-            : Collections.emptyList();
-    long recordCount =
-        columnStats.stream().map(ColumnStat::getNumValues).max(Long::compareTo).orElse(0L);
-    // TODO(https://github.com/onetable-io/onetable/issues/102): removed record count.
-    return OneDataFile.builder()
-        .physicalPath(getFullPathToFile(deltaSnapshot, addFile.path()))
-        .fileFormat(fileFormat)
-        .fileSizeBytes(addFile.size())
-        .lastModified(addFile.modificationTime())
-        .partitionValues(
-            partitionExtractor.partitionValueExtraction(addFile.partitionValues(), partitionFields))
-        .columnStats(columnStats)
-        .recordCount(recordCount)
-        .build();
-  }
-
-  public OneDataFile convertRemoveActionToOneDataFile(
-      RemoveFile removeFile,
-      Snapshot deltaSnapshot,
-      FileFormat fileFormat,
-      List<OnePartitionField> partitionFields,
-      DeltaPartitionExtractor partitionExtractor) {
-    return OneDataFile.builder()
-        .physicalPath(getFullPathToFile(deltaSnapshot, removeFile.path()))
-        .fileFormat(fileFormat)
-        .partitionValues(
-            partitionExtractor.partitionValueExtraction(
-                removeFile.partitionValues(), partitionFields))
-        .build();
-  }
-
-  public FileFormat convertToOneTableFileFormat(String provider) {
-    if (provider.equals("parquet")) {
-      return FileFormat.APACHE_PARQUET;
-    } else if (provider.equals("orc")) {
-      return FileFormat.APACHE_ORC;
-    }
-    throw new NotSupportedException(
-        String.format("delta file format %s is not recognized", provider));
-  }
-
-  static String getFullPathToFile(Snapshot snapshot, String dataFilePath) {
-    String tableBasePath = snapshot.deltaLog().dataPath().toUri().toString();
-    if (dataFilePath.startsWith(tableBasePath)) {
-      return dataFilePath;
-    }
-    return tableBasePath + Path.SEPARATOR + dataFilePath;
-  }
-}

core/src/main/java/io/onetable/delta/DeltaIncrementalChangesState.java
@@ -1,104 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
-package io.onetable.delta;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-
-import lombok.Builder;
-
-import org.apache.spark.sql.delta.DeltaLog;
-import org.apache.spark.sql.delta.actions.Action;
-
-import scala.Tuple2;
-import scala.collection.JavaConverters;
-import scala.collection.Seq;
-
-import com.google.common.base.Preconditions;
-
-/** Cache store for storing incremental table changes in the Delta table. */
-public class DeltaIncrementalChangesState {
-  private final Long startVersion;
-  private final Long endVersion;
-
-  private final Map<Long, List<Action>> incrementalChangesByVersion = new HashMap<>();
-
-  /**
-   * Reloads the cache store with incremental changes. Intentionally thread safety is the
-   * responsibility of the caller.
-   *
-   * @param deltaLog The DeltaLog instance.
-   * @param versionToStartFrom The version to start from.
-   */
-  @Builder
-  public DeltaIncrementalChangesState(DeltaLog deltaLog, Long versionToStartFrom) {
-    // TODO (https://github.com/onetable-io/onetable/issues/103) Fall back to snapshot sync in
-    // vacuum cases.
-    List<Tuple2<Long, List<Action>>> changesList =
-        getChangesList(deltaLog.getChanges(versionToStartFrom, false));
-    Long maxSeenVersion = null;
-    for (Tuple2<Long, List<Action>> change : changesList) {
-      Long versionNumber = change._1();
-      List<Action> actions = change._2();
-      incrementalChangesByVersion.put(versionNumber, actions);
-      maxSeenVersion =
-          maxSeenVersion == null ? versionNumber : Math.max(maxSeenVersion, versionNumber);
-    }
-    startVersion = versionToStartFrom;
-    endVersion = maxSeenVersion;
-  }
-
-  /**
-   * Returns the versions in sorted order. The start version is the next one after the last sync
-   * version to the target client. The end version is the latest version in the Delta table at the
-   * time of initialization.
-   *
-   * @return
-   */
-  public List<Long> getVersionsInSortedOrder() {
-    List<Long> versions = new ArrayList<>(incrementalChangesByVersion.keySet());
-    versions.sort(Long::compareTo);
-    return versions;
-  }
-
-  public List<Action> getActionsForVersion(Long version) {
-    Preconditions.checkArgument(
-        incrementalChangesByVersion.containsKey(version),
-        String.format("Version %s not found in the DeltaIncrementalChangesState.", version));
-    return incrementalChangesByVersion.get(version);
-  }
-
-  private List<Tuple2<Long, List<Action>>> getChangesList(
-      scala.collection.Iterator<Tuple2<Object, Seq<Action>>> scalaIterator) {
-    List<Tuple2<Long, List<Action>>> changesList = new ArrayList<>();
-    Iterator<Tuple2<Object, Seq<Action>>> javaIterator =
-        JavaConverters.asJavaIteratorConverter(scalaIterator).asJava();
-    while (javaIterator.hasNext()) {
-      Tuple2<Object, Seq<Action>> currentChange = javaIterator.next();
-      changesList.add(
-          new Tuple2<>(
-              (Long) currentChange._1(),
-              JavaConverters.seqAsJavaListConverter(currentChange._2()).asJava()));
-    }
-    return changesList;
-  }
-}

core/src/test/java/io/onetable/testutil/Issues.java
@@ -1,25 +1,0 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
-package io.onetable.testutil;
-
-public class Issues {
-
-  /** Refer to <a href="https://github.com/onetable-io/onetable/issues/102">GitHub #102</a> */
-  public static final boolean ISSUE_102_FIXED = false;
-}

commit d8b9d644407410f1b9e466681211c5f4942c5478

Fix broken links after move to Apache and rename from onetable (#384)
This fixes some URL breakage produced by #374

core/src/main/java/org/apache/xtable/delta/DeltaActionsConverter.java
@@ -61,7 +61,7 @@ public class DeltaActionsConverter {
             : Collections.emptyList();
     long recordCount =
         columnStats.stream().map(ColumnStat::getNumValues).max(Long::compareTo).orElse(0L);
-    // TODO(https://github.com/onetable-io/onetable/issues/102): removed record count.
+    // TODO(https://github.com/apache/incubator-xtable/issues/102): removed record count.
     return OneDataFile.builder()
         .physicalPath(getFullPathToFile(deltaSnapshot, addFile.path()))
         .fileFormat(fileFormat)

core/src/main/java/org/apache/xtable/delta/DeltaIncrementalChangesState.java
@@ -51,8 +51,6 @@ public class DeltaIncrementalChangesState {
    */
   @Builder
   public DeltaIncrementalChangesState(DeltaLog deltaLog, Long versionToStartFrom) {
-    // TODO (https://github.com/onetable-io/onetable/issues/103) Fall back to snapshot sync in
-    // vacuum cases.
     List<Tuple2<Long, List<Action>>> changesList =
         getChangesList(deltaLog.getChanges(versionToStartFrom, false));
     Long maxSeenVersion = null;

core/src/test/java/org/apache/xtable/delta/ITDeltaSourceClient.java
@@ -317,7 +317,6 @@ public class ITDeltaSourceClient {
     DeltaSourceClient client = clientProvider.getSourceClientInstance(tableConfig);
     // Get current snapshot
     OneSnapshot snapshot = client.getCurrentSnapshot();
-    // TODO: Complete and enable test (see https://github.com/onetable-io/onetable/issues/90)
   }
 
   @ParameterizedTest

core/src/test/java/org/apache/xtable/testutil/Issues.java
@@ -20,6 +20,6 @@ package org.apache.xtable.testutil;
 
 public class Issues {
 
-  /** Refer to <a href="https://github.com/onetable-io/onetable/issues/102">GitHub #102</a> */
+  /** Refer to <a href="https://github.com/apache/incubator-xtable/issues/102">GitHub #102</a> */
   public static final boolean ISSUE_102_FIXED = false;
 }