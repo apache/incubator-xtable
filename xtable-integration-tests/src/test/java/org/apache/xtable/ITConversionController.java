e2f50b704f4f13c0dce73c0076cbad0f553291cd
Timothy Brown <tim@onehouse.ai>
2025-01-13 20:14:09 -0600

    add jackson-scala for delta dependency

--- xtable-integration-tests/src/test/java/org/apache/xtable/ITConversionController.java
+++ xtable-integration-tests/src/test/java/org/apache/xtable/ITConversionController.java
@@ -70,6 +70,7 @@ import org.apache.hudi.common.config.HoodieMetadataConfig;
 import org.apache.hudi.common.model.HoodieAvroPayload;
 import org.apache.hudi.common.model.HoodieRecord;
 import org.apache.hudi.common.model.HoodieTableType;
+import org.apache.hudi.common.table.timeline.HoodieInstant;
 
 import org.apache.iceberg.Table;
 import org.apache.iceberg.hadoop.HadoopTables;
@@ -96,7 +97,6 @@ import org.apache.xtable.hudi.TestSparkHudiTable;
 import org.apache.xtable.iceberg.IcebergConversionSourceProvider;
 import org.apache.xtable.model.storage.TableFormat;
 import org.apache.xtable.model.sync.SyncMode;
-import org.apache.xtable.shade.org.apache.hudi.common.table.timeline.HoodieInstant;
 import org.apache.xtable.shade.org.apache.iceberg.Snapshot;
 
 public class ITConversionController {
