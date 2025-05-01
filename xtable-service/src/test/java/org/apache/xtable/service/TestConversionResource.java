dae612927c2b13c378aec5ffd615d4b12219a8b2
Rahil Chertara <rchertara@gmail.com>
2025-05-01 16:32:36 -0700

    Add unit test for conversion resource

---
+++ xtable-service/src/test/java/org/apache/xtable/service/TestConversionResource.java
@@ -0,0 +1,77 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.apache.xtable.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import org.apache.xtable.service.models.ConvertTableRequest;
+import org.apache.xtable.service.models.ConvertTableResponse;
+import org.apache.xtable.service.models.RestTargetTable;
+
+@ExtendWith(MockitoExtension.class)
+class TestConversionResource {
+
+  private static final String SOURCE_TABLE_BASE_PATH = "s3://bucket/tables/users";
+  private static final String TARGET_ICEBERG_METADATA_PATH = "s3://bucket/tables/users/metadata";
+
+  @Mock private ConversionService conversionService;
+
+  @InjectMocks private ConversionResource resource;
+
+  @Test
+  void convertTable_delegatesToService_andReturnsExpectedResponse() {
+    ConvertTableRequest req =
+        ConvertTableRequest.builder()
+            .sourceFormat("DELTA")
+            .sourceTableName("users")
+            .sourceTablePath(SOURCE_TABLE_BASE_PATH)
+            .targetFormats(Arrays.asList("ICEBERG"))
+            .build();
+
+    RestTargetTable icebergTable =
+        RestTargetTable.builder()
+            .targetFormat("ICEBERG")
+            .targetMetadataPath(TARGET_ICEBERG_METADATA_PATH)
+            .build();
+
+    ConvertTableResponse expected =
+        ConvertTableResponse.builder().conversions(Arrays.asList(icebergTable)).build();
+    when(conversionService.convertTable(req)).thenReturn(expected);
+    ConvertTableResponse actual = resource.convertTable(req);
+    verify(conversionService).convertTable(req);
+
+    assertNotNull(actual);
+    assertSame(expected, actual, "Resource should return the exact response from the service");
+
+    assertEquals(1, actual.getConversions().size());
+    assertEquals("ICEBERG", actual.getConversions().get(0).getTargetFormat());
+    assertEquals(
+        TARGET_ICEBERG_METADATA_PATH, actual.getConversions().get(0).getTargetMetadataPath());
+  }
+}
