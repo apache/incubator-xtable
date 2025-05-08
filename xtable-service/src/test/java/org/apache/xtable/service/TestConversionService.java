f98f5690b3b90d7d082a80f369d588cbe56614b4
Rahil Chertara <rchertara@gmail.com>
2025-05-07 17:24:28 -0700

    minor mvn fix

--- xtable-service/src/test/java/org/apache/xtable/service/TestConversionService.java
+++ xtable-service/src/test/java/org/apache/xtable/service/TestConversionService.java
@@ -37,7 +37,6 @@ package org.apache.xtable.service;
  */
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
