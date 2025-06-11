c6379b594054bfbe2f73e4381ec713eb989e2d8f
vaibhavk1992 <kumarvaibhav1992@ymail.com>
2025-06-11 20:53:07 +0530

    adding Kernel 4.0 code

--- xtable-core/src/test/java/org/apache/xtable/DeltaTableKernel.java
+++ xtable-core/src/test/java/org/apache/xtable/DeltaTableKernel.java
@@ -15,105 +15,97 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 package org.apache.xtable;
 
 // import org.junit.jupiter.api.Test;
 //
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.junit.jupiter.api.Test;
-import java.util.Optional;
-import java.io.IOException;
-
-import io.delta.kernel.*;
-import io.delta.kernel.defaults.*;
-import org.apache.hadoop.conf.Configuration;
-import io.delta.kernel.engine.Engine;
-import io.delta.kernel.defaults.engine.DefaultEngine;
-import io.delta.kernel.types.StructType;
-import io.delta.kernel.data.Row;
-import io.delta.kernel.utils.CloseableIterator;
-import io.delta.kernel.data.FilteredColumnarBatch;
-import io.delta.kernel.internal.data.ScanStateRow;
-import io.delta.kernel.utils.FileStatus;
-import io.delta.kernel.internal.InternalScanFileUtils;
-import io.delta.kernel.data.ColumnarBatch;
-import io.delta.kernel.data.ColumnVector;
 import static io.delta.kernel.internal.util.Utils.singletonCloseableIterator;
 
+import java.io.IOException;
+import java.util.Optional;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.delta.kernel.*;
+import io.delta.kernel.data.ColumnVector;
+import io.delta.kernel.data.ColumnarBatch;
+import io.delta.kernel.data.FilteredColumnarBatch;
+import io.delta.kernel.data.Row;
+import io.delta.kernel.defaults.*;
+import io.delta.kernel.defaults.engine.DefaultEngine;
+import io.delta.kernel.engine.Engine;
+import io.delta.kernel.internal.InternalScanFileUtils;
+import io.delta.kernel.internal.data.ScanStateRow;
+import io.delta.kernel.types.StructType;
+import io.delta.kernel.utils.CloseableIterator;
+import io.delta.kernel.utils.FileStatus;
+
 public class DeltaTableKernel {
-    private static final Logger logger = LoggerFactory.getLogger(DeltaTableKernel.class);
-      @Test
-  public void readDeltaKernel() throws IOException{
-            String myTablePath ="/Users/vaibhakumar/Desktop/opensource/iceberg/warehouse/demo/nyc/taxis"; // fully qualified
-            Configuration hadoopConf = new Configuration();
-            Engine myEngine = DefaultEngine.create(hadoopConf);
-            Table myTable = Table.forPath(myEngine, myTablePath);
-            Snapshot mySnapshot = myTable.getLatestSnapshot(myEngine);
-            long version = mySnapshot.getVersion(myEngine);
-            StructType tableSchema = mySnapshot.getSchema(myEngine);
-            Scan myScan = mySnapshot.getScanBuilder(myEngine).build();
+  private static final Logger logger = LoggerFactory.getLogger(DeltaTableKernel.class);
+
+  @Test
+  public void readDeltaKernel() throws IOException {
+    String myTablePath =
+        "/Users/vaibhakumar/Desktop/opensource/iceberg/warehouse/demo/nyc/taxis"; // fully qualified
+    Configuration hadoopConf = new Configuration();
+    Engine myEngine = DefaultEngine.create(hadoopConf);
+    Table myTable = Table.forPath(myEngine, myTablePath);
+    Snapshot mySnapshot = myTable.getLatestSnapshot(myEngine);
+    long version = mySnapshot.getVersion();
+    StructType tableSchema = mySnapshot.getSchema();
+    Scan myScan = mySnapshot.getScanBuilder().build();
 
     // Common information about scanning for all data files to read.
-            Row scanState = myScan.getScanState(myEngine);
+    Row scanState = myScan.getScanState(myEngine);
 
     // Information about the list of scan files to read
-            CloseableIterator<FilteredColumnarBatch> fileIter = myScan.getScanFiles(myEngine);
-          int readRecordCount = 0;
-          try {
-              StructType physicalReadSchema =
-                      ScanStateRow.getPhysicalDataReadSchema(myEngine, scanState);
-              while (fileIter.hasNext()) {
-                  FilteredColumnarBatch scanFilesBatch = fileIter.next();
-                  try (CloseableIterator<Row> scanFileRows = scanFilesBatch.getRows()) {
-                      while (scanFileRows.hasNext()) {
-                          Row scanFileRow = scanFileRows.next();
-                          FileStatus fileStatus =
-                                  InternalScanFileUtils.getAddFileStatus(scanFileRow);
-                          CloseableIterator<ColumnarBatch> physicalDataIter =
-                                  myEngine.getParquetHandler().readParquetFiles(
-                                          singletonCloseableIterator(fileStatus),
-                                          physicalReadSchema,
-                                          Optional.empty());
-                          try (
-                                  CloseableIterator<FilteredColumnarBatch> transformedData =
-                                          Scan.transformPhysicalData(
-                                                  myEngine,
-                                                  scanState,
-                                                  scanFileRow,
-                                                  physicalDataIter)) {
-                              while (transformedData.hasNext()) {
-                                  FilteredColumnarBatch logicalData = transformedData.next();
-                                  ColumnarBatch dataBatch = logicalData.getData();
+    CloseableIterator<FilteredColumnarBatch> fileIter = myScan.getScanFiles(myEngine);
+    int readRecordCount = 0;
+    try {
+      StructType physicalReadSchema = ScanStateRow.getPhysicalDataReadSchema(myEngine, scanState);
+      while (fileIter.hasNext()) {
+        FilteredColumnarBatch scanFilesBatch = fileIter.next();
+        try (CloseableIterator<Row> scanFileRows = scanFilesBatch.getRows()) {
+          while (scanFileRows.hasNext()) {
+            Row scanFileRow = scanFileRows.next();
+            FileStatus fileStatus = InternalScanFileUtils.getAddFileStatus(scanFileRow);
+            CloseableIterator<ColumnarBatch> physicalDataIter =
+                myEngine
+                    .getParquetHandler()
+                    .readParquetFiles(
+                        singletonCloseableIterator(fileStatus),
+                        physicalReadSchema,
+                        Optional.empty());
+            try (CloseableIterator<FilteredColumnarBatch> transformedData =
+                Scan.transformPhysicalData(myEngine, scanState, scanFileRow, physicalDataIter)) {
+              while (transformedData.hasNext()) {
+                FilteredColumnarBatch logicalData = transformedData.next();
+                ColumnarBatch dataBatch = logicalData.getData();
 
+                // access the data for the column at ordinal 0
+                ColumnVector column0 = dataBatch.getColumnVector(0);
+                ColumnVector column1 = dataBatch.getColumnVector(1);
+                ColumnVector column2 = dataBatch.getColumnVector(2);
+                ColumnVector column3 = dataBatch.getColumnVector(3);
 
-                                  // access the data for the column at ordinal 0
-                                  ColumnVector column0 = dataBatch.getColumnVector(0);
-                                  ColumnVector column1 = dataBatch.getColumnVector(1);
-                                  ColumnVector column2 = dataBatch.getColumnVector(2);
-                                  ColumnVector column3 = dataBatch.getColumnVector(3);
-
-                                  for (int rowIndex = 0; rowIndex < column0.getSize() ; rowIndex++) {
-                                      System.out.println(column0.getInt(rowIndex));
-
-                                  }
-                                  for (int rowIndex = 0; rowIndex < column1.getSize() ; rowIndex++) {
-                                      System.out.println(column1.getString(rowIndex));
-
-                                  }
-                              }
-                          }
-                      }
-                  }
+                for (int rowIndex = 0; rowIndex < column0.getSize(); rowIndex++) {
+                  System.out.println(column0.getInt(rowIndex));
+                }
+                for (int rowIndex = 0; rowIndex < column1.getSize(); rowIndex++) {
+                  System.out.println(column1.getString(rowIndex));
+                }
               }
-          } catch (IOException e)
-          {
-              e.printStackTrace();
-              System.out.println("IOException occurred: " + e.getMessage());
+            }
           }
-
-
-
+        }
       }
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.out.println("IOException occurred: " + e.getMessage());
+    }
+  }
 }
