5ffe5e6cbfa44e24c387763cdbb7eacd3cf171ba
Selim Soufargi <ssoufargi.idealab.unical@gmail.com~>
2025-06-24 21:59:02 +0200

    file changes + refactoring of conversion source

--- xtable-api/src/main/java/org/apache/xtable/model/schema/PartitionFieldSpec.java
+++ xtable-api/src/main/java/org/apache/xtable/model/schema/PartitionFieldSpec.java
@@ -15,14 +15,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+ 
 package org.apache.xtable.model.schema;
 
 import lombok.Value;
 
-
 @Value
 public class PartitionFieldSpec {
-    String sourceFieldPath;
-    PartitionTransformType transformType;
-    String format;
+  String sourceFieldPath;
+  PartitionTransformType transformType;
+  String format;
 }
--- xtable-core/src/main/java/org/apache/xtable/parquet/ParquetPartitionValueExtractor.java
+++ xtable-core/src/main/java/org/apache/xtable/parquet/ParquetPartitionValueExtractor.java
@@ -18,88 +18,64 @@
  
 package org.apache.xtable.parquet;
 
-import java.time.Instant;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
-import org.apache.parquet.schema.MessageType;
-import org.apache.xtable.model.schema.InternalField;
-import org.apache.xtable.model.schema.InternalPartitionField;
-import org.apache.xtable.model.schema.InternalSchema;
-import org.apache.xtable.model.schema.InternalType;
-import org.apache.xtable.model.stat.PartitionValue;
-import org.apache.parquet.hadoop.metadata.ParquetMetadata;
-import org.apache.xtable.model.stat.Range;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Map;
-import java.util.regex.Pattern;
-
 import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TimeZone;
 
-import lombok.AllArgsConstructor;
-import lombok.NonNull;
 import lombok.Value;
+
+import org.apache.parquet.hadoop.metadata.ParquetMetadata;
+import org.apache.parquet.schema.MessageType;
 
 import org.apache.xtable.exception.PartitionValuesExtractorException;
 import org.apache.xtable.model.schema.InternalPartitionField;
+import org.apache.xtable.model.schema.InternalSchema;
 import org.apache.xtable.model.schema.InternalType;
 import org.apache.xtable.model.stat.PartitionValue;
 import org.apache.xtable.model.stat.Range;
 
-
 /** Partition value extractor for Parquet. */
 // Extracts the partitionFields and values and create InputParitionFields object (fields and types)
-//then convert those to InternalPartitionField for the ConversionSource
-//@NoArgsConstructor(access = AccessLevel.PRIVATE)
+// then convert those to InternalPartitionField for the ConversionSource
+// @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ParquetPartitionValueExtractor {
   private static final OffsetDateTime EPOCH = Instant.ofEpochSecond(0).atOffset(ZoneOffset.UTC);
   private static final ParquetPartitionValueExtractor INSTANCE =
       new ParquetPartitionValueExtractor();
   private static final ParquetSchemaExtractor schemaExtractor =
-          ParquetSchemaExtractor.getInstance();
+      ParquetSchemaExtractor.getInstance();
   private static final ParquetMetadataExtractor parquetMetadataExtractor =
-          ParquetMetadataExtractor.getInstance();
+      ParquetMetadataExtractor.getInstance();
 
   private static final ParquetPartitionSpecExtractor partitionsSpecExtractor =
-          ParquetPartitionSpecExtractor.getInstance();
+      ParquetPartitionSpecExtractor.getInstance();
 
   public static ParquetPartitionValueExtractor getInstance() {
     return INSTANCE;
   }
+
   private static final String HIVE_DEFAULT_PARTITION = "__HIVE_DEFAULT_PARTITION__";
   private final Map<String, String> pathToPartitionFieldFormat = null;
 
-
   public List<InternalPartitionField> extractParquertPartitions(
-          ParquetMetadata footer, String path) {
-    MessageType parquetSchema  = parquetMetadataExtractor.getSchema(footer);
+      ParquetMetadata footer, String path) {
+    MessageType parquetSchema = parquetMetadataExtractor.getSchema(footer);
     InternalSchema internalSchema = schemaExtractor.toInternalSchema(parquetSchema, path);
     List<InternalPartitionField> partitions = partitionsSpecExtractor.spec(internalSchema);
     return partitions;
   }
 
-
-
   public List<PartitionValue> extractPartitionValues(
-          List<InternalPartitionField> partitionColumns, String partitionPath) {
+      List<InternalPartitionField> partitionColumns, String partitionPath) {
     if (partitionColumns == null) {
       return Collections.emptyList();
     }
@@ -116,42 +92,42 @@ public class ParquetPartitionValueExtractor {
       PartialResult valueAndRemainingPath;
       if (remainingPartitionPath.startsWith(HIVE_DEFAULT_PARTITION)) {
         String remaining =
-                remainingPartitionPath.length() > HIVE_DEFAULT_PARTITION.length()
-                        ? remainingPartitionPath.substring(HIVE_DEFAULT_PARTITION.length() + 1)
-                        : "";
+            remainingPartitionPath.length() > HIVE_DEFAULT_PARTITION.length()
+                ? remainingPartitionPath.substring(HIVE_DEFAULT_PARTITION.length() + 1)
+                : "";
         valueAndRemainingPath = new PartialResult(null, remaining);
       } else {
         valueAndRemainingPath =
-                parsePartitionPath(partitionField, remainingPartitionPath, totalNumberOfPartitions);
+            parsePartitionPath(partitionField, remainingPartitionPath, totalNumberOfPartitions);
       }
       result.add(
-              PartitionValue.builder()
-                      .partitionField(partitionField)
-                      .range(Range.scalar(valueAndRemainingPath.getValue()))
-                      .build());
+          PartitionValue.builder()
+              .partitionField(partitionField)
+              .range(Range.scalar(valueAndRemainingPath.getValue()))
+              .build());
       remainingPartitionPath = valueAndRemainingPath.getRemainingPath();
     }
     return result;
   }
 
   private PartialResult parsePartitionPath(
-          InternalPartitionField field, String remainingPath, int totalNumberOfPartitions) {
+      InternalPartitionField field, String remainingPath, int totalNumberOfPartitions) {
     switch (field.getTransformType()) {
       case YEAR:
       case MONTH:
       case DAY:
       case HOUR:
         return parseDate(
-                remainingPath, pathToPartitionFieldFormat.get(field.getSourceField().getPath()));
+            remainingPath, pathToPartitionFieldFormat.get(field.getSourceField().getPath()));
       case VALUE:
         // if there is only one partition field, then assume full partition path is used even if
         // value contains slashes
         boolean isSlashDelimited = totalNumberOfPartitions > 1;
         return parseValue(
-                remainingPath, field.getSourceField().getSchema().getDataType(), isSlashDelimited);
+            remainingPath, field.getSourceField().getSchema().getDataType(), isSlashDelimited);
       default:
         throw new IllegalArgumentException(
-                "Unexpected partition type: " + field.getTransformType());
+            "Unexpected partition type: " + field.getTransformType());
     }
   }
 
@@ -162,16 +138,16 @@ public class ParquetPartitionValueExtractor {
       simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
       long dateAsEpochMillis = simpleDateFormat.parse(dateString).toInstant().toEpochMilli();
       return new PartialResult(
-              dateAsEpochMillis,
-              remainingPath.substring(Math.min(remainingPath.length(), format.length() + 1)));
+          dateAsEpochMillis,
+          remainingPath.substring(Math.min(remainingPath.length(), format.length() + 1)));
     } catch (ParseException ex) {
       throw new PartitionValuesExtractorException(
-              "Unable to parse date from path: " + remainingPath, ex);
+          "Unable to parse date from path: " + remainingPath, ex);
     }
   }
 
   private static PartialResult parseValue(
-          String remainingPath, InternalType sourceFieldType, boolean isSlashDelimited) {
+      String remainingPath, InternalType sourceFieldType, boolean isSlashDelimited) {
     if (remainingPath.isEmpty()) {
       throw new PartitionValuesExtractorException("Missing partition value");
     }
@@ -184,7 +160,7 @@ public class ParquetPartitionValueExtractor {
     }
     String valueAsString = remainingPath.substring(0, endCharIndex);
     String unParsedPath =
-            remainingPath.substring(Math.min(endCharIndex + 1, remainingPath.length()));
+        remainingPath.substring(Math.min(endCharIndex + 1, remainingPath.length()));
     Object parsedValue;
     switch (sourceFieldType) {
       case STRING:
@@ -219,7 +195,7 @@ public class ParquetPartitionValueExtractor {
         break;
       default:
         throw new PartitionValuesExtractorException(
-                "Unexpected source field type in partition parser: " + sourceFieldType);
+            "Unexpected source field type in partition parser: " + sourceFieldType);
     }
     return new PartialResult(parsedValue, unParsedPath);
   }
