c39c998ec947f5681454f68510a8152536665460
Timothy Brown <tim@onehouse.ai>
2026-02-23 12:23:58 -0500

    update xtable-service module

--- xtable-service/src/test/java/org/apache/xtable/service/TestConversionResource.java
+++ xtable-service/src/test/java/org/apache/xtable/service/TestConversionResource.java
@@ -18,6 +18,9 @@
  
 package org.apache.xtable.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
