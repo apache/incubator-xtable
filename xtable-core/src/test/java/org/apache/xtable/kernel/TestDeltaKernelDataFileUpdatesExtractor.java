d4058703ad79cc2baf5ebc436bcbf003a865aac0
vaibhavk1992 <kumarvaibhav1992@ymail.com>
2026-03-02 22:18:06 +0530

    adding read write integration test case

--- xtable-core/src/test/java/org/apache/xtable/kernel/TestDeltaKernelDataFileUpdatesExtractor.java
+++ xtable-core/src/test/java/org/apache/xtable/kernel/TestDeltaKernelDataFileUpdatesExtractor.java
@@ -18,7 +18,6 @@
  
 package org.apache.xtable.kernel;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
