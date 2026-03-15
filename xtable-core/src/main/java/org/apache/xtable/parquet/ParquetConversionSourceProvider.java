4d636e034e77e31c916d76284a8fed3153e488d7
Selim Soufargi <ssoufargi.idealab.unical@gmail.com~>
2026-03-15 16:44:53 +0100

    spotless:apply and test fix

--- xtable-core/src/main/java/org/apache/xtable/parquet/ParquetConversionSourceProvider.java
+++ xtable-core/src/main/java/org/apache/xtable/parquet/ParquetConversionSourceProvider.java
@@ -32,12 +32,14 @@ public class ParquetConversionSourceProvider extends ConversionSourceProvider<Lo
     ParquetPartitionValueExtractor partitionValueExtractor =
         new ParquetPartitionValueExtractor(
             sourcePartitionSpecExtractor.getPathToPartitionFieldFormat());
+    ParquetDataManager dataManager = new ParquetDataManager(this.hadoopConf, sourceTable.getBasePath());
     return ParquetConversionSource.builder()
         .tableName(sourceTable.getName())
         .basePath(sourceTable.getBasePath())
         .hadoopConf(this.hadoopConf)
         .partitionSpecExtractor(sourcePartitionSpecExtractor)
         .partitionValueExtractor(partitionValueExtractor)
+        .parquetDataManager(dataManager)
         .build();
   }
 }
